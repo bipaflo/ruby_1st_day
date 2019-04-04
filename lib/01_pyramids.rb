c5b3b6903243a334eb9c9338806236208dbb7a8f
paulfloyd <paulbitsindou@gmail.com>
2019-04-04 22:57:27 +0200

    pyramides

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,12 +1,25 @@
-def full_pyramid
+def wtf_pyramid
 puts " Salut, bienvenue dans ma super pyramide ! Combien d'étages veux tu ?"
+print ">"
 y = gets.chomp.to_i
-i=1
-while i <= y
-  full_pyramid = ("#" * (2*i-1))
-  puts full_pyramid.center(full_pyramid.to_i)
-  i += 1
-end
-end
 
-full_pyramid
+
+  i=1
+   while i <= (y-1)/2
+
+       wtf_pyramid = ("#" * (2*i-1)).center(y)
+
+       puts wtf_pyramid
+       i += 1
+     end
+
+     end
+     i=(y-1)/2
+     while i>=1
+       wtf_pyramid= ("#" * (2*i-1)).center(y)
+       puts wtf_pyramid
+       i-=1
+
+     end
+
+wtf_pyramid
