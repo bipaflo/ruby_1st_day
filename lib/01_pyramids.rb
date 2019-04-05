bfd5913d91efcdb4fd6881304650a5127611fcec
paulfloyd <paulbitsindou@gmail.com>
2019-04-05 04:31:14 +0200

    pyramid final

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,25 +1,31 @@
-def wtf_pyramid
+def wtf_pyramyd
 puts " Salut, bienvenue dans ma super pyramide ! Combien d'étages veux tu ?"
 print ">"
 y = gets.chomp.to_i
-
-
+x = y/2
+unless x = gets.chomp.to_i
   i=1
-   while i <= (y-1)/2
+   while i <= (y+1)/2
 
-       wtf_pyramid = ("#" * (2*i-1)).center(y)
+    wtf_pyramyd = ("#" * (2*i-1)).center(y)
 
-       puts wtf_pyramid
+       puts wtf_pyramyd
        i += 1
      end
+   else
+     puts "Un nombre impair stp??"
+ end
+ def wtflow_pyramid
+  i=(y-1)/2
+  while i >= 1
+    wtflow_pyramid= ("#" * (2*i-1)).center(y)
+    puts wtflow_pyramid
+    i -= 1
+  end
+end
+  def perform
 
-     end
-     i=(y-1)/2
-     while i>=1
-       wtf_pyramid= ("#" * (2*i-1)).center(y)
-       puts wtf_pyramid
-       i-=1
-
-     end
-
-wtf_pyramid
+     wtf_pyramid
+     wtflow_pyramid(y)
+   end
+   perform
